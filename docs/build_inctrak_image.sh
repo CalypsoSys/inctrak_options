f5940f07d192a92f5ec1cfa760f44f71be4ee790
Joe <joseph@calypsosys.com>
2024-06-02 23:25:45 -0400

    misc cleanup

--- docs/build_inctrak_image.sh
+++ docs/build_inctrak_image.sh
@@ -1,4 +1,4 @@
-1. cd /home/calypso/dotnet/inctrak_options
+1. cd ~/dotnet/inctrak_options
 2. git pull
 3. cd shared.inctrak.com/
 4. docker build -t inctrak .
@@ -7,15 +7,15 @@
 7. Copy to local machine (host)
 
 1. Transfer inctrak.tar to production and update .lastgood
-2. cd /home/calypso/docker/inctrak_demo_dotnet
+2. cd ~/docker/inctrak_demo_dotnet
 3. docker-compose stop
-4. cd /home/calypso/docker/inctrak_shared_dotnet
+4. cd ~/docker/inctrak_shared_dotnet
 5. docker-compose stop
 6. cd ..
 7. docker load < inctrak.tar
 8. docker rmi $(docker images -f "dangling=true" -q)
-9. cd /home/calypso/docker/inctrak_demo_dotnet
+9. cd ~/docker/inctrak_demo_dotnet
 10. docker-compose start
-11. cd /home/calypso/docker/inctrak_shared_dotnet
+11. cd ~/docker/inctrak_shared_dotnet
 12. docker-compose start
 
